9ae6da4b71177ee533ab3683dfd60d38b3edde7d
Adarsh M. Nair <adarshvaikhary@gmail.com>
2026-06-13 12:58:18 +0530

    frontend folders and route initialized

--- backend/api/main.py
+++ backend/api/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from api.routes.player_routes import router as player_router
 from api.routes.team_routes import router as team_router
@@ -6,32 +7,43 @@ from api.routes.match_routes import router as match_router
 from api.routes.stats_routes import router as stats_router
 from api.routes.rag_routes import router as rag_router
 
-# ---------------------------------------------------
-# Create FastAPI App
-# ---------------------------------------------------
-
 app = FastAPI(
     title="Cricket Intelligence Agent API",
     description="Cricket Analytics and AI Backend",
     version="1.0.0"
 )
 
+# -----------------------------------------
+# CORS
+# -----------------------------------------
 
-# ---------------------------------------------------
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# -----------------------------------------
 # Root Endpoint
-# ---------------------------------------------------
+# -----------------------------------------
 
 @app.get("/")
 def home():
-
     return {
         "message": "Cricket Intelligence Agent API is running"
     }
 
+@app.get("/health")
+def health():
+    return {
+        "status": "ok"
+    }
 
-# ---------------------------------------------------
+# -----------------------------------------
 # Register API Routes
-# ---------------------------------------------------
+# -----------------------------------------
 
 app.include_router(player_router)
 app.include_router(team_router)
